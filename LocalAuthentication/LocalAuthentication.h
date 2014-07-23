c9a51b8978e41154c89ab9cb5b0793e8e907a60b
Joseph Duffy <JosephDuffy@me.com>
2014-07-23 20:21:39 +0100

    Initial Commit
    Default Cocoa Touch Swift Framework, other than Deployment Target, set at iOS 7.0

---
+++ LocalAuthentication/LocalAuthentication.h
@@ -0,0 +1,19 @@
+//
+//  LocalAuthentication.h
+//  LocalAuthentication
+//
+//  Created by Joseph Duffy on 23/07/2014.
+//  Copyright (c) 2014 Yetii Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LocalAuthentication.
+FOUNDATION_EXPORT double LocalAuthenticationVersionNumber;
+
+//! Project version string for LocalAuthentication.
+FOUNDATION_EXPORT const unsigned char LocalAuthenticationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocalAuthentication/PublicHeader.h>
+
+
